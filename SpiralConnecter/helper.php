1ab1f31b381c59a3af3a97849dd7c6dfcdb4b2e4
APKAVIFL7EWRG4EKB4U4 <ito.shun@pi-pe.co.jp>
2024-04-11 14:46:48 +0900

    first commit

---
+++ SpiralConnecter/helper.php
@@ -0,0 +1,6 @@
+<?php
+
+function spiral(){
+    global $SPIRAL;
+    return $SPIRAL;
+}
---
+++ SpiralConnecter/require.php
@@ -0,0 +1,17 @@
+<?php 
+require_once 'SpiralConnecter/Request/HttpRequestParameter.php';
+require_once 'SpiralConnecter/XSpiralApiHeaderObject.php';
+require_once 'SpiralConnecter/SpiralConnecterInterface.php';
+require_once 'SpiralConnecter/SpiralManager.php';
+require_once 'SpiralConnecter/OrderBy.php';
+require_once 'SpiralConnecter/Paginator.php';
+require_once 'SpiralConnecter/Request/HttpRequest.php';
+require_once 'SpiralConnecter/SpiralApiConnecter.php';
+require_once 'SpiralConnecter/SpiralConnecter.php';
+require_once 'SpiralConnecter/SpiralDB.php';
+require_once 'SpiralConnecter/SpiralExpressManager.php';
+require_once 'SpiralConnecter/SpiralFilterManager.php';
+require_once 'SpiralConnecter/SpiralRedis.php';
+require_once 'SpiralConnecter/SpiralWeb.php';
+require_once 'SpiralConnecter/SpiralWebManager.php';
+require_once 'SpiralConnecter/helper.php';
